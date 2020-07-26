bb988ce93b75016ba5232e8fbb251655a4742c41
kohey127 <koooochoki@gmail.com>
2020-07-26 18:51:38 +0900

    [3-9]Finish

--- vagrant/sample_app/app/controllers/todolists_controller.rb
+++ vagrant/sample_app/app/controllers/todolists_controller.rb
@@ -1,42 +1,34 @@
 class TodolistsController < ApplicationController
   def new
-  	@list = List.new
+    @list = List.new
   end
-
   def create
-  	# 1.データを新規登録するためのインスタンス作成
-  	list = List.new(list_params)
-  	# 2.データをデータベースに保存するためのsaveメソッド実行
-  	list.save
-  	# 3.詳細画面へリダイレクト
-  	redirect_to todolist_path(list.id)
+    list = List.new(list_params)
+    list.save
+    redirect_to todolist_path(list.id)
   end
-
   def index
-  	@lists = List.all
+    @lists = List.all
   end
-
   def show
-  	@list = List.find(params[:id])
+    @list = List.find(params[:id])
   end
-
   def edit
-  	@list = List.find(params[:id])
+    @list = List.find(params[:id])
   end
-
   def update
-  	list = List.find(params[:id])
-  	list.update(list_params)
-  	redirect_to todolist_path(list.id)
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to todolist_path(list.id)
   end
-
   def destroy
-  	
+    list = List.find(params[:id]) #データ(レコード)を1件取得
+    list.destroy #データ（レコード）を削除
+    redirect_to todolists_path #List一覧画面へリダイレクト
   end
 
-  # ストロングパラメータ
   private
   def list_params
-  	params.require(:list).permit(:title, :body, :image)
+    params.require(:list).permit(:title, :body, :image)
   end
 end
